54b8e52b2921c1cbe8274c96bcd8376efabfa3e0
ahros-code <probroalpha@gmail.com>
2024-09-21 15:44:06 +0500

    pushsh

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   const searchParams = useSearchParams();
   const searchParam = searchParams.get("search");
   const locale = useLocale();
-  const [isInfo, setIsInfo] = useState(false);
+  const [isInfo, setIsInfo] = useState(false); // State to toggle info box
   const [stickyNav, setStickyNav] = useState(false);
   const [search, setSearch] = useState(searchParam ? searchParam : "");
   const router = useRouter();
@@ -66,6 +66,7 @@ const Navbar = () => {
   const col2 = useTranslations("Col2");
   const col3 = useTranslations("Col3");
   const col4 = useTranslations("Col4");
+
   return (
     <header className={`flex justify-center items-center    z-[100]`}>
       <nav
@@ -97,150 +98,84 @@ const Navbar = () => {
             placeholder={`${s("search")} ...`}
             className=" flex-grow bg-transparent focus:outline-none text-sm text-gray-500"
           />
-          <div className=" relative">
-            <div className="cursor-pointer md:hidden block m-0 p-0 h-5 w-5">
-              <Sheet>
-                <SheetTrigger>
-                  <div>
-                    <MessageCircleQuestionIcon strokeWidth={1} size={20} />
-                  </div>
-                </SheetTrigger>
-                <SheetContent side="top">
-                  <SheetHeader>
-                    <SheetDescription className="z-[999]">
-                      <table className="w-full border-collapse border border-blue-500 max-w-xl mt-8 mx-auto">
-                        <thead>
-                          <tr className="bg-blue-500 text-white">
-                            <th className="py-2 px-4 text-left w-10">
-                              {col1("name")}
-                            </th>
-                            <th className="py-2 px-4 text-left">
-                              {col2("name")}
-                            </th>
-                            <th className="py-2 px-4 text-left">
-                              {col3("name")}
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr className="bg-white border-b border-blue-500">
-                            <td className="py-2 px-4 text-red-500">
-                              {`"`}
-                              <span className="text-black">
-                                {col1("text_1")}
-                              </span>
-                              {`"`}
-                            </td>
-                            <td className="py-2 px-4">{col1("text_2")}</td>
-                            <td className="py-2 px-4">{col1("text_3")}</td>
-                          </tr>
-                          <tr className="bg-white border-b border-blue-500">
-                            <td className="py-2 px-4 text-red-500">
-                              <span className="text-black">
-                                {col2("text_1")}
-                              </span>
-                              {`*`}
-                            </td>
-                            <td className="py-2 px-4">{col2("text_2")}</td>
-                            <td className="py-2 px-4">{col2("text_3")}</td>
-                          </tr>
-                          <tr className="bg-white border-b border-blue-500">
-                            <td className="py-2 px-4 text-red-500">
-                              {`*`}
-                              <span className="text-black">
-                                {col3("text_1")}
-                              </span>
-                            </td>
-                            <td className="py-2 px-4">{col3("text_2")}</td>
-                            <td className="py-2 px-4">L{col3("text_3")}</td>
-                          </tr>
-
-                          <tr className="bg-white border-b border-blue-500">
-                            <td className="py-2 px-4 text-red-500">
-                              {`*`}
-                              <span className="text-black">
-                                {col4("text_1")}
-                              </span>
-                              {`*`}
-                            </td>
-                            <td className="py-2 px-4">{col4("text_2")}</td>
-                            <td className="py-2 px-4">{col4("text_3")}</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </SheetDescription>
-                  </SheetHeader>
-                </SheetContent>
-              </Sheet>
-            </div>
-
+          <div className="relative">
+            {/* Wrapping the icon and info table in a common parent */}
             <div
-              className="cursor-pointer md:block hidden"
               onMouseEnter={() => setIsInfo(true)}
-              onMouseLeave={() => setIsInfo(true)}
+              onMouseLeave={() => setIsInfo(false)}
             >
-              <MessageCircleQuestionIcon strokeWidth={1} size={20} />
-            </div>
-            {isInfo && (
-              <div
-                onMouseEnter={() => setIsInfo(true)}
-                onMouseLeave={() => setIsInfo(false)}
-                className="absolute top-10 right-0 cursor-pointer w-auto h-auto shadow  bg-white z-50 flex justify-center items-center rounded-tl-[100px] rounded-bl-[100px] rounded-br-[100px] overflow-hidden border-4 border-blue-500"
-              >
-                <div className="w-max h-full px-10 p-5">
-                  <table className="w-full mx-auto">
-                    <thead>
-                      <tr>
-                        <th className="py-2 px-4 text-left ">{col1("name")}</th>
-                        <th className="py-2 px-4 text-left">{col2("name")}</th>
-                        <th className="py-2 px-4 text-left">{col3("name")}</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr className="bg-white border-b border-blue-500">
-                        <td className="py-2 px-4 text-red-500">
-                          {`"`}
-                          <span className="text-black">{col1("text_1")}</span>
-                          {`"`}
-                        </td>
-                        <td className="py-2 px-4">{col1("text_2")}</td>
-                        <td className="py-2 px-4">
-                          {`"`}
-                          <span>{col1("text_3")}</span>
-                          {`"`}
-                        </td>
-                      </tr>
-                      <tr className="bg-white border-b border-blue-500">
-                        <td className="py-2 px-4 text-red-500">
-                          <span className="text-black">{col2("text_1")}</span>
-                          {`*`}
-                        </td>
-                        <td className="py-2 px-4">{col2("text_2")}</td>
-                        <td className="py-2 px-4">{col2("text_3")}</td>
-                      </tr>
-                      <tr className="bg-white border-b border-blue-500">
-                        <td className="py-2 px-4 text-red-500">
-                          {`*`}
-                          <span className="text-black">{col3("text_1")}</span>
-                        </td>
-                        <td className="py-2 px-4">{col3("text_2")}</td>
-                        <td className="py-2 px-4">{col3("text_3")}</td>
-                      </tr>
-
-                      <tr className="bg-white border-b border-blue-500">
-                        <td className="py-2 px-4 text-red-500">
-                          {`*`}
-                          <span className="text-black">{col4("text_1")}</span>
-                          {`*`}
-                        </td>
-                        <td className="py-2 px-4">{col4("text_2")}</td>
-                        <td className="py-2 px-4">{col4("text_3")}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+              {/* MessageCircleQuestionIcon */}
+              <div className="cursor-pointer md:block hidden">
+                <MessageCircleQuestionIcon strokeWidth={1} size={20} />
               </div>
-            )}
+
+              {/* Info table that appears when hovering */}
+              {isInfo && (
+                <div
+                  onMouseEnter={() => setIsInfo(true)}
+                  onMouseLeave={() => setIsInfo(false)}
+                  className="absolute top-10 right-0 cursor-pointer w-auto h-auto shadow bg-white z-50 flex justify-center items-center rounded-tl-[100px] rounded-bl-[100px] rounded-br-[100px] overflow-hidden border-4 border-blue-500"
+                >
+                  <div className="w-[800px] h-full px-10 p-5">
+                    <table className="w-full mx-auto">
+                      <thead>
+                        <tr>
+                          <th className="py-2 px-4 text-left ">
+                            {col1("name")}
+                          </th>
+                          <th className="py-2 px-4 text-left">
+                            {col2("name")}
+                          </th>
+                          <th className="py-2 px-4 text-left">
+                            {col3("name")}
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr className="bg-white border-b border-blue-500">
+                          <td className="py-2 px-4 text-red-500">
+                            {`"`}
+                            <span className="text-black">{col1("text_1")}</span>
+                            {`"`}
+                          </td>
+                          <td className="py-2 px-4">{col1("text_2")}</td>
+                          <td className="py-2 px-4">
+                            {`"`}
+                            <span>{col1("text_3")}</span>
+                            {`"`}
+                          </td>
+                        </tr>
+                        <tr className="bg-white border-b border-blue-500">
+                          <td className="py-2 px-4 text-red-500">
+                            <span className="text-black">{col2("text_1")}</span>
+                            {`*`}
+                          </td>
+                          <td className="py-2 px-4">{col2("text_2")}</td>
+                          <td className="py-2 px-4">{col2("text_3")}</td>
+                        </tr>
+                        <tr className="bg-white border-b border-blue-500">
+                          <td className="py-2 px-4 text-red-500">
+                            {`*`}
+                            <span className="text-black">{col3("text_1")}</span>
+                          </td>
+                          <td className="py-2 px-4">{col3("text_2")}</td>
+                          <td className="py-2 px-4">{col3("text_3")}</td>
+                        </tr>
+                        <tr className="bg-white border-b border-blue-500">
+                          <td className="py-2 px-4 text-red-500">
+                            {`*`}
+                            <span className="text-black">{col4("text_1")}</span>
+                            {`*`}
+                          </td>
+                          <td className="py-2 px-4">{col4("text_2")}</td>
+                          <td className="py-2 px-4">{col4("text_3")}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
 
@@ -291,26 +226,22 @@ const Navbar = () => {
                           key={item.name}
                           data-aos="fade-left"
                           data-aos-delay={(i + 1) * 100}
-                          data-aos-duration={(i + 1) * 100}
+                          data-aos-duration="400"
                         >
                           <SheetClose asChild>
                             <Link
                               href={`/${locale}${item.slug}`}
-                              className={`md:p-3 py-2 flex gap-2 ${
+                              className={`${
                                 item.slug == "/" + pathname.split("/")[2]
                                   ? "text-maincolor"
                                   : "text-black"
-                              } items-center hover:text-maincolor duration-300`}
+                              } block px-2 py-1 hover:text-maincolor duration-300`}
                             >
-                              {item.icon}
-                              <p>{t(`${i}`)}</p>
+                              {t(`${i}`)}
                             </Link>
                           </SheetClose>
                         </li>
                       ))}
-                      <div className="block md:hidden text-end w-full">
-                        <LocalSwitcher />
-                      </div>
                     </ul>
                   </SheetDescription>
                 </SheetHeader>
